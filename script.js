d0711f3b57f1447b8fc7e63dbd012d7faa91ecd6
BalajiGaneshK <kbalajiganeshdav@gmail.com>
2021-08-17 14:41:09 +0530

    access key edited

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let conversionRates = {};
 let currency1 = "INR";
 let currency2 = "USD";
 let lastUpdatedDate;
+let allHistoricData;
 let flag = 0;
 
 //Fetching Conversion rates data from API
 
-fetch("https://api.exchangeratesapi.io/latest").then(
+fetch("http://api.exchangeratesapi.io/v1/latest?access_key=02d91e97b9cbb8b598c5e284cedeb705").then(
 
     response => {
         return response;
@@ -21,7 +22,7 @@ fetch("https://api.exchangeratesapi.io/latest").then(
             data => {
                 Object.assign(conversionRates, data.rates);
                 lastUpdatedDate = data.date;
-                console.log(data);
+                //console.log(data);
             }
         )
             
@@ -34,6 +35,37 @@ fetch("https://api.exchangeratesapi.io/latest").then(
 
     error => console.error(error)
 )
+/*
+//Fetch historical Conversion rates based on input
+async function getHistoricalConversionRates(startDate, endDate)
+{
+    let api_url = "https://api.exchangeratesapi.io/history?start_at=" + startDate + "&end_at=" + endDate;
+    let data2 = await fetch(api_url);
+    allHistoricData = await data2.json();
+     
+}
+
+//Get toCurrency value of 1 unit of fromCurrency 
+function getInputBasedHistoricData(fromCurrency, toCurrency,allHistoricCoversionRates)
+{
+    
+    let result = {};
+    let keys = Object.keys(allHistoricCoversionRates);
+    for (let i = 0; i < keys.length; i++)
+    {
+        let value = allHistoricCoversionRates[i][toCurrency] / allHistoricCoversionRates[i][fromCurrency];
+        result[keys[i]] = value;
+
+    }
+    return result;
+
+}
+
+getHistoricalConversionRates("2021-02-01", "2021-02-12");
+console.log("allHistoricData:", allHistoricData);
+let specificHistoricData =  getInputBasedHistoricData(currency1, currency2, allHistoricData.rates);
+console.log(specificHistoricData);
+*/
 
 /*
 
@@ -41,7 +73,7 @@ IMPORTANT NOTE: conversionRates{} have base currency as EUR.
 Each value in the object is the value of 1 EUR in the respective country's currency 
 
 */
-console.log(conversionRates);
+//console.log(conversionRates);
 
 convert = (id) => {                                             // the id from which user has given input
     
@@ -111,12 +143,16 @@ convert = (id) => {                                             // the id from w
 
         lastUpdatedDate = tempDate[1]+ " "+ tempDate[2] + " th " + tempDate[0];
         console.log(lastUpdatedDate);
-    }  
+    } 
+    
+    //Displaying Last Updated Date info in UI
     document.getElementById("last-updated-info").innerHTML = "Last Updated On "+ lastUpdatedDate;
     
+    //Computing toCurrencyValue 
     let toCurrencyValue = (fromCurrencyValue / conversionRates[fromCurrency]) * conversionRates[toCurrency];
     document.getElementById(targetId).value = toCurrencyValue;
 
+    //Displaying the Results in
     document.getElementById("input-data").innerHTML = fromCurrencyValue + `  ` + fromCurrency+ "  equals"    ;
     document.getElementById("output-data").innerHTML =  toCurrencyValue.toFixed(2)+ `  ` + toCurrency  ;
 
